3b71741ffd3bd95ca57734b47d118696fca384ba
Dhairy Tripathi <hexdecimal16@gmail.com>
2022-04-29 19:41:24 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { LocalDataSource } from 'angular2-smart-table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  settings = {
+    columns: {
+      name: {
+        title: 'Name',
+      },
+      gender: {
+        title: 'Gender',
+      },
+    },
+  };
+  data = [
+    {
+      name: 'Leanne Graham',
+      gender: 'male',
+    },
+  ];
   title = 'test';
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    //  Testing bug https://github.com/dj-fiorex/angular2-smart-table/issues/31
+    this.source.load(this.data).then();
+    this.source.empty().then();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Angular2SmartTableModule } from 'angular2-smart-table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, Angular2SmartTableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
